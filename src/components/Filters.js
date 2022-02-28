cd4b5fa1d2ed30d313c7b2e529309594b75074a0
marleigh <volcana@gmail.com>
2022-02-27 19:38:18 -0800

    creating custom radio component

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,47 +1,35 @@
+import { nanoid } from "nanoid";
+import Radio from "./Radio";
+
 export default function Filters(props) {
        
    const handleChange = (e) => {
         props.setDataFilters(e.target.value);
     }
 
-    //REFACTOR IDEA : create radio button component
+    //add government to this array eventually - currently a bug where it breaks the API call for unknown reasons
+        //check, maybe it takes too long to load?
+
+    const filterTypes = ["community", "research"];
+
+    const radioDisplay = filterTypes.map(radio => {
+        return <Radio
+                    name="filter"
+                    value={radio}
+                    handleChange={handleChange}
+                    dataFilters={props.dataFilters}
+                    key={nanoid()}
+                />
+    });
+
     return (
         <section className="filters">
+            {radioDisplay}
             {/* 
                     BUG:
                         -government entity search breaks
                             -API call seems identical - not sure why
-    
             */}
-            
-            {/* <div>
-                <input type="radio" 
-                    name="filter"
-                    value="government"
-                    checked={props.dataFilters === "government"}
-                    onChange={handleChange}
-                    />
-                <label htmlFor="government">Government</label>
-            </div> */}
-            
-            <div>
-                <input type="radio" 
-                    name="filter" 
-                    value="research"
-                    checked={props.dataFilters === "research"}
-                    onChange={handleChange}
-                    />
-                <label htmlFor="research">Research</label>
-            </div>
-            <div>
-                <input type="radio" 
-                    name="filter"
-                    value="community"
-                    checked={props.dataFilters === "community"}
-                    onChange={handleChange}
-                    />
-                <label htmlFor="community">Community</label>
-            </div>
         </section>
     )
 }
